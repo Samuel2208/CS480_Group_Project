ee6680334d728081cce797b4a234b4fac00ff34e
sharay2 <sharay2@uic.edu>
2025-04-30 23:09:48 -0500

    Updated gitignore and links to some html pages

--- models.py
+++ models.py
@@ -7,6 +7,13 @@ class Person(db.Model):
     name = db.Column(db.String(255), nullable=False)
     age = db.Column(db.Integer, nullable=False)
 
+# Manager class with info that a manager would have
+class Manager(db.Model):
+    __tablename__ = 'managers'
+    ssn = db.Column(db.String(11), primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+
 # test
 def __repr__(self):
     return f"Person with id: {self.id}, name: {self.name}, and age: {self.age}"
--- routes.py
+++ routes.py
@@ -1,8 +1,62 @@
 from flask import render_template, request
-from models import Person
+from models import Person, Manager
 
 def register_routes(app, db):
+    # Routing back to the home page
     @app.route('/')
     def index():
         people = Person.query.all()
         return render_template('index.html', people=people)
+
+    @app.route('/login-client')
+    def login_client():
+        return render_template('login_client.html')
+
+    @app.route('/login-driver')
+    def login_driver():
+        return render_template('login_driver.html')
+
+
+    # Routing to registering a new manager screen
+    @app.route('/register-manager', methods=['GET', 'POST'])
+    def register_manager():
+        if request.method == 'POST':
+            name = request.form['name']
+            ssn = request.form['ssn']
+            email = request.form['email']
+            
+            new_manager = Manager(name=name, ssn=ssn, email=email)
+            db.session.add(new_manager)
+            db.session.commit()
+
+            return f"""
+                <h2>✅ Manager {name} registered successfully!</h2>
+                <form action="/manager-dashboard/{ssn}" method="get">
+                    <button type="submit">Continue to Dashboard</button>
+                </form>
+                <p><a href="/">Return to Home</a></p>
+                    """
+
+        return render_template('register_manager.html')
+    
+    # Logging in as a manager
+    @app.route('/login-manager', methods=['GET', 'POST'])
+    def login_manager():
+        if request.method == 'POST':
+            ssn = request.form['ssn']
+            manager = Manager.query.filter_by(ssn=ssn).first()
+            if manager:
+                return redirect(f"/manager-dashboard/{ssn}")
+            else:
+                return "Invalid SSN. Manager not found.", 404
+
+        return render_template('login_manager.html')
+
+    
+    # Manager dashboard screen
+    @app.route('/manager-dashboard/<ssn>')
+    def manager_dashboard(ssn):
+        manager = Manager.query.filter_by(ssn=ssn).first()
+        if not manager:
+            return "Manager not found", 404
+        return render_template('manager_dashboard.html', manager=manager)
